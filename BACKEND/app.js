2cc70c4314cbf2cfc69a97ee248bc4f67285582a
rohit <rohitsin5312@gmail.com>
2026-07-02 22:37:39 +0530

    Fix filename casing for Linux deployment

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -5,7 +5,7 @@ import urlSchema from "./src/models/short_url.model.js"
 import connectDB from "./src/config/mongo.config.js";
 import short_url from './src/routes/short_url.route.js';
 import { redirectFromShortUrl } from './src/controller/short_url.controller.js';
-import { errorHandler } from './src/utils/errorhandler.js';
+import { errorHandler } from './src/utils/errorHandler.js';
 import cors from "cors";
 import auth_routes from "./src/routes/auth.routes.js"
 import user_routes from "./src/routes/user.routes.js"
