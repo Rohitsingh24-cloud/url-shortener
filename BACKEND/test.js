1c9637dd1d140535413c2b408c89c04dc8c1f09f
Rohit Singh <rohitsin5312@gmail.com>
2026-07-07 19:15:34 +0530

    Remove mongoose connection logic from test.js
    Removed mongoose connection code from test.js.

--- BACKEND/test.js
+++ BACKEND/test.js
@@ -1,13 +1,1 @@
-import mongoose from "mongoose";
 
-const uri =
-
-
-try {
-  await mongoose.connect(uri);
-  console.log("Connected!");
-  process.exit(0);
-} catch (err) {
-  console.error(err);
-  process.exit(1);
-}
